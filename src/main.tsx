124b90a847677f58c7aa76fb91de9c2d5a9e65e3
jane-spothub <techspothubke@gmail.com>
2026-09-03 12:26:42 +0300

    feat: mount router + auth/online/query providers in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'react-hot-toast'
+import { AuthProvider } from './contexts/AuthContext'
+import { OnlineProvider } from './contexts/OnlineContext'
 import App from './App.tsx'
+import './index.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 1, refetchOnWindowFocus: false } },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <OnlineProvider>
+            <App />
+            <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
+          </OnlineProvider>
+        </AuthProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
